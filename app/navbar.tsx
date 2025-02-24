43b77d08f9c5989c840365a9970db1129c5de5c0
Isuru Karunaratna <93091809+isuruK2003@users.noreply.github.com>
2025-02-24 08:58:20 +0530

    fix: navbar green color removed

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -19,7 +19,7 @@ export default function Navbar() {
   }, [menuOpen]);
 
   return (
-    <header className="sticky top-0 z-50 w-full border-b bg-green-300/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <header className="sticky top-0 z-50 w-full border-b bg-white-300/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-14 items-center justify-between px-4 md:px-8">
         {/* Logo */}
         <div className="flex items-center gap-2 font-semibold">
