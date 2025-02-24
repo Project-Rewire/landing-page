24efa3c23d6dd666e6676f7d61baf71bc35f575c
Isuru Karunaratna <93091809+isuruK2003@users.noreply.github.com>
2025-02-24 23:23:32 +0530

    feat: vercel speed insights added

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,9 @@ import "./globals.css"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import type React from "react"
+
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -28,6 +30,7 @@ export default function RootLayout({
       <body className={inter.className}>
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
